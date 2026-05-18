c293fc7a6c7f6c29d40c7eb4fefbdeef9886babc
frutall-connect <frutall.info@gmail.com>
2026-05-18 02:10:42 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
 import withPWA from 'next-pwa'
 
 const nextConfig = {
+
   reactStrictMode: true,
+
+  turbopack: {},
+
 }
 
 export default withPWA({
+
   dest: 'public',
+
   register: true,
+
   skipWaiting: true,
+
 })(nextConfig)
