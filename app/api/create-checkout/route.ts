f708e4aad0f348bdae98df955713e7e5e3f70ab6
frutall-connect <frutall.info@gmail.com>
2026-05-22 01:56:42 +0200

    Update route.ts

--- app/api/create-checkout/route.ts
+++ app/api/create-checkout/route.ts
@@ -9,6 +9,11 @@ export async function POST(
 
   try {
 
+console.log(
+  'STRIPE KEY:',
+  process.env.STRIPE_SECRET_KEY
+)
+
     const stripe = new Stripe(
 
       process.env.STRIPE_SECRET_KEY || '',
