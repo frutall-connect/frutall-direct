4f8e56d7d7352a5f2c43625c44ec1c13a6cbe443
frutall-connect <frutall.info@gmail.com>
2026-05-22 00:42:58 +0200

    Update page.tsx

--- app/carrito/page.tsx
+++ app/carrito/page.tsx
@@ -90,8 +90,10 @@ export default function CarritoPage() {
     if (pedidoError || !pedidoData) {
 
       console.error(pedidoError)
-console.log(pedidoError)
-      alert('Error creando pedido')
+
+      console.log(pedidoError)
+      
+alert('Error creando pedido')
 
       return
 
