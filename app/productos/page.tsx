b36dc0545e576eecba1ee604cbe7c0a551b70bab
frutall-connect <frutall.info@gmail.com>
2026-05-27 18:38:19 +0200

    Update page.tsx

--- app/productos/page.tsx
+++ app/productos/page.tsx
@@ -17,21 +17,7 @@ import { useSearchParams } from 'next/navigation'
 
 export default function ProductosPage({
 
-  searchParams
-
-}: {
-
-  searchParams: {
-
-    categoria?: string
-
-  }
-
-}) {
-
-  const [productos, setProductos] = useState<any[]>([])
-
-  const [busqueda, setBusqueda] = useState('')
+{
 
 const searchParams =
   useSearchParams()
