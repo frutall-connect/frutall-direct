5953ce259daaf451df25c9211947109e2ed73e2a
frutall-connect <frutall.info@gmail.com>
2026-05-28 22:57:27 +0200

    app

---
+++ components/home/AppleActions.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+import Link from 'next/link'
+
+type Props = {
+  totalProductos: number
+  repetirUltimoPedido: () => void
+}
+
+export default function AppleActions({
+  totalProductos,
+  repetirUltimoPedido
+}: Props) {
+
+  return (
+
+    <div
+      className="
+        grid
+        grid-cols-2
+        gap-3
+        mt-6
+      "
+    >
+
+      {/* REPETIR */}
+
+      <button
+        onClick={repetirUltimoPedido}
+        className="
+          h-[58px]
+          rounded-[1.5rem]
+          bg-green-700
+          text-white
+          font-black
+          text-[0.98rem]
+          shadow-lg
+          active:scale-[0.98]
+          transition
+        "
+      >
+        Repetir pedido
+      </button>
+
+      {/* CARRITO */}
+
+      <Link href="/carrito">
+
+        <button
+          className="
+            w-full
+            h-[58px]
+            rounded-[1.5rem]
+            bg-black
+            text-white
+            font-black
+            text-[0.98rem]
+            shadow-lg
+            active:scale-[0.98]
+            transition
+          "
+        >
+          Carrito ({totalProductos})
+        </button>
+
+      </Link>
+
+    </div>
+
+  )
+
+}
---
+++ components/home/AppleOffer.tsx
@@ -0,0 +1,161 @@
+'use client'
+
+export default function AppleOffer() {
+
+  return (
+
+    <div className="mt-6">
+
+      {/* TITULO */}
+
+      <div
+        className="
+          flex
+          items-center
+          justify-between
+          mb-3
+        "
+      >
+
+        <h2
+          className="
+            text-[1.35rem]
+            font-black
+            tracking-tight
+            text-black
+          "
+        >
+          Oferta del día
+        </h2>
+
+        <span
+          className="
+            text-green-700
+            text-[1.5rem]
+            font-light
+          "
+        >
+          →
+        </span>
+
+      </div>
+
+      {/* CARD */}
+
+      <div
+        className="
+          bg-white
+          rounded-[2rem]
+          p-3
+          shadow-sm
+          border
+          border-gray-100
+        "
+      >
+
+        <div
+          className="
+            flex
+            gap-4
+            items-center
+          "
+        >
+
+          {/* IMAGEN */}
+
+          <img
+
+            src="
+https://images.unsplash.com/photo-1519996529931-28324d5a630e?q=80&w=1200&auto=format&fit=crop
+"
+
+            alt="Oferta"
+
+            className="
+              w-[92px]
+              h-[92px]
+              rounded-[1.5rem]
+              object-cover
+            "
+
+          />
+
+          {/* INFO */}
+
+          <div className="flex-1">
+
+            <div
+              className="
+                inline-flex
+                items-center
+                justify-center
+                px-3
+                py-1
+                rounded-full
+                bg-red-500
+                text-white
+                text-[0.7rem]
+                font-black
+              "
+            >
+              -20%
+            </div>
+
+            <h3
+              className="
+                text-[1.15rem]
+                leading-[1.2rem]
+                font-black
+                mt-3
+                text-black
+              "
+            >
+              Uva Roja
+              <br />
+              Sin Semilla
+            </h3>
+
+            <div
+              className="
+                flex
+                items-end
+                gap-2
+                mt-3
+              "
+            >
+
+              <span
+                className="
+                  text-[1.7rem]
+                  leading-none
+                  font-black
+                  text-green-700
+                "
+              >
+                1,80€
+              </span>
+
+              <span
+                className="
+                  text-gray-400
+                  line-through
+                  text-[0.9rem]
+                  mb-[2px]
+                "
+              >
+                2,30€
+              </span>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  )
+
+}
