de15aa8ba4535e54b7d8636ccf28b0db79264565
frutall-connect <frutall.info@gmail.com>
2026-05-28 23:33:15 +0200

    Update AppleCategories.tsx

--- components/home/AppleCategories.tsx
+++ components/home/AppleCategories.tsx
@@ -6,6 +6,15 @@ type Props = {
   categorias: any[]
 }
 
+const iconos: Record<string, string> = {
+
+  Frutas: '🍎',
+  Verduras: '🥬',
+  Tubérculos: '🥔',
+  Temporada: '🍅'
+
+}
+
 export default function AppleCategories({
   categorias
 }: Props) {
@@ -21,81 +30,79 @@ export default function AppleCategories({
       "
     >
 
-      {categorias.map(
-        (categoria) => (
+      {categorias.map((categoria) => (
 
-          <Link
-            key={categoria.id}
-            href={`/productos?categoria=${categoria.nombre}`}
+        <Link
+          key={categoria.id}
+          href={`/productos?categoria=${categoria.nombre}`}
+        >
+
+          <div
+            className="
+              bg-white
+              rounded-[1.8rem]
+              px-4
+              py-4
+              h-[108px]
+              border
+              border-[#ececec]
+              shadow-[0_4px_12px_rgba(0,0,0,0.05)]
+              flex
+              flex-col
+              justify-between
+            "
           >
+
+            {/* ICONO */}
 
             <div
               className="
-                bg-white
-                rounded-[1.7rem]
-                p-4
-                shadow-sm
-                border
-                border-gray-100
-                h-[108px]
+                text-[2rem]
+                leading-none
+              "
+            >
+              {iconos[categoria.nombre] || '📦'}
+            </div>
+
+            {/* FOOTER */}
+
+            <div
+              className="
                 flex
-                flex-col
+                items-center
                 justify-between
-                active:scale-[0.98]
-                transition
               "
             >
 
-              {/* ICONO */}
-
-              <div
+              <span
                 className="
-                  text-[2rem]
+                  text-[1rem]
+                  font-bold
+                  text-black
+                  tracking-[-0.02em]
+                "
+              >
+                {categoria.nombre}
+              </span>
+
+              <span
+                className="
+                  text-gray-400
+                  text-[1.5rem]
                   leading-none
+                  font-light
                 "
               >
-                {categoria.icono}
-              </div>
-
-              {/* TEXTO */}
-
-              <div
-                className="
-                  flex
-                  items-center
-                  justify-between
-                "
-              >
-
-                <span
-                  className="
-                    text-[1rem]
-                    font-black
-                    tracking-tight
-                    text-black
-                  "
-                >
-                  {categoria.nombre}
-                </span>
-
-                <span
-                  className="
-                    text-gray-400
-                    text-[1.3rem]
-                    font-light
-                  "
-                >
-                  →
-                </span>
-
-              </div>
+                →
+              </span>
 
             </div>
 
-          </Link>
+          </div>
 
-        )
-      )}
+        </Link>
+
+      ))}
 
     </div>
 
