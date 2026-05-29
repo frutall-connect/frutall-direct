b10b5ec0dafd5dcbe48f6af996b5a51f716db4be
frutall-connect <frutall.info@gmail.com>
2026-05-29 18:14:25 +0200

    Update AppleCategories.tsx

--- components/home/AppleCategories.tsx
+++ components/home/AppleCategories.tsx
@@ -50,8 +50,18 @@ export default function AppleCategories() {
 
           <div
             className="
-  bg-white/75
-  backdrop-blur-md
+              min-w-[74px]
+              h-[92px]
+              bg-white
+              rounded-[1.5rem]
+              border
+              border-[#ededed]
+              shadow-[0_4px_10px_rgba(0,0,0,0.05)]
+              px-3
+              py-3
+              flex
+              flex-col
+              justify-between
             "
           >
 
