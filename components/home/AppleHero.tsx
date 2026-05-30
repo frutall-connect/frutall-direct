62c7a96e3fad13180c822772009e16c9039ff3b4
frutall-connect <frutall.info@gmail.com>
2026-05-30 08:19:26 +0200

    Update AppleHero.tsx

--- components/home/AppleHero.tsx
+++ components/home/AppleHero.tsx
@@ -54,7 +54,7 @@ export default function AppleHero({
         text-white
         font-black
         tracking-[-0.05em]
-        text-[2rem]
+        text-[1rem]
         leading-[1.95rem]
         drop-shadow-xl
       "
