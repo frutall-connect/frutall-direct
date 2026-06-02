fcbad50ee53be5cb7d60d2522f0d70b5295db795
frutall-connect <frutall.info@gmail.com>
2026-06-02 21:08:51 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,30 +2,6 @@ import './globals.css'
 
 import AppleHeader from '@/components/home/AppleHeader'
 
-export default function MobileLayout({
-  children
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <>
-      <div
-        className="
-          bg-white
-          w-full
-          px-4
-          pt-3
-          pb-2
-        "
-      >
-        <AppleHeader />
-      </div>
-
-      {children}
-    </>
-  )
-}
-
 export const metadata = {
 
   title: 'FrutALL Direct',
