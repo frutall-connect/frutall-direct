bc0b27f134701bd9aae5fc38b6b6a1f824c0d413
frutall-connect <frutall.info@gmail.com>
2026-09-03 13:10:18 +0200

    fix: hide admin panel for non-admin users

--- app/cuenta/page.tsx
+++ app/cuenta/page.tsx
@@ -66,8 +66,6 @@ export default function CuentaPage() {
 
   }
 
-const esAdmin = true
-
   return (
 
     <MobileLayout>
@@ -281,47 +279,6 @@ const esAdmin = true
           </div>
 
         )}
-
-{
-
-  esAdmin && (
-
-    <a
-
-      href="/admin"
-
-      className="
-        mt-6
-        bg-black
-        text-white
-        rounded-3xl
-        p-5
-        flex
-        items-center
-        justify-between
-        text-xl
-        font-bold
-      "
-
-    >
-
-      <span>
-
-        ⚙️ Panel Admin
-
-      </span>
-
-      <span>
-
-        →
-
-      </span>
-
-    </a>
-
-  )
-
-}
 
         {/* LOGOUT */}
 
